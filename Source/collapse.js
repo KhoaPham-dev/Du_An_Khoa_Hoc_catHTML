cb28071029bef2af690c201a5226259372c316e1
KhoaPham-dev <superstars1860@gmail.com>
2021-02-03 22:13:20 +0700

    first commit

---
+++ Source/collapse.js
@@ -0,0 +1,4 @@
+document.getElementById("nav-collapse").addEventListener("click", function (e) {
+  document.querySelector("ul.ul.mb").classList.toggle("ul-active");
+  document.querySelector("nav").classList.toggle("nav-active");
+});
