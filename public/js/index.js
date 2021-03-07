61827482849c723b5210333c6864cc04fca23445
KhoaPham-dev <superstars1860@gmail.com>
2021-03-07 12:52:22 +0700

    trang ca nhan va cai dat

--- public/js/index.js
+++ public/js/index.js
@@ -527,3 +527,60 @@ if (document.querySelectorAll("body.hoconline").length > 0) {
   }
 }
 //#endregion HOC ONLINE
+
+//#region TRANG CA NHAN VA CAI DAT
+if (document.querySelectorAll("body.tcnvcd").length > 0) {
+  // Slider
+  $(".tcnvcd.slider-content1").slick({
+    dots: true,
+    speed: 500,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    rows: 2,
+    responsive: [
+      {
+        breakpoint: 815,
+        settings: {
+          rows: 1,
+          slidesToShow: 2,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          rows: 3,
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          slidesPerRow: 1,
+        },
+      },
+    ],
+  });
+  $(".tcnvcd.slider-content2").slick({
+    dots: true,
+    speed: 500,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    responsive: [
+      {
+        breakpoint: 815,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          centerMode: true,
+        },
+      },
+    ],
+  });
+}
+//#endregion TRANG CA NHAN VA CAI DAT
