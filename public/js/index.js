c51c3114274314afc0708f192d60898f84b6bcb1
KhoaPham-dev <superstars1860@gmail.com>
2021-03-09 01:37:49 +0700

    trang danh sach tin tuc

--- public/js/index.js
+++ public/js/index.js
@@ -584,3 +584,67 @@ if (document.querySelectorAll("body.tcnvcd").length > 0) {
   });
 }
 //#endregion TRANG CA NHAN VA CAI DAT
+
+//#region DANH SACH TIN TUC
+if (document.querySelectorAll("body.dstt").length > 0) {
+  //SLIDER
+  $(".dstt.slider-content4").slick({
+    dots: true,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    autoplay: true,
+    autoplaySpeed: 2500,
+    responsive: [
+      {
+        breakpoint: 815,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          centerMode: true,
+        },
+      },
+    ],
+  });
+  $(".dstt.slider-1").slick({
+    dots: true,
+    infinite: false,
+    speed: 300,
+    slidesToShow: 3.5,
+    slidesToScroll: 3,
+    centerMode: false,
+  });
+
+  $(".dstt.slider-2").slick({
+    dots: true,
+    infinite: false,
+    speed: 300,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    responsive: [
+      {
+        breakpoint: 815,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 1.2,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  });
+}
+//#endregion DANH SACH TIN TUC
