4b3332127d4635c1a15e0a660ebb69f9ddd85e45
KhoaPham-dev <superstars1860@gmail.com>
2021-03-10 14:39:56 +0700

    dang-ky-khoa-hoc

---
+++ public/dang-ky-khoa-hoc/slideshow.js
@@ -0,0 +1,16 @@
+$(".slider-content1").slick({
+  dots: true,
+  infinite: false,
+  speed: 300,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  responsive: [
+    {
+      breakpoint: 815,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+      },
+    },
+  ],
+});
